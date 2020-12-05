1172cf9fa9e4a016016477df8b16cc67e3b23019
l08084 <l08084.1989@gmail.com>
2020-12-05 17:27:37 +0900

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   count = 0;
   count2 = 0;
+  count3 = 0;
   constructor() {
-    this.count2 = 'ABC'.length; // 3
-    this.count = '😀🏴󠁧󠁢󠁷󠁬󠁳󠁿😇'.length; // 18
+    // 例1. 通常の文字
+    this.count2 = 'A'.length; // 1
+    // 例2. 通常の絵文字
+    this.count = '😀'.length; // 2
+    // 例3. 特殊な絵文字
+    this.count3 = '🏴󠁧󠁢󠁷󠁬󠁳󠁿'.length; // 14
   }
 }
