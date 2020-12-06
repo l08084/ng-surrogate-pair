e509f62f49765b28580a621a3e96e7ffa52a0e74
l08084 <l08084.1989@gmail.com>
2020-12-06 19:27:53 +0900

    add surrogate-pair directive

--- src/app/surrogate-pair-max-length.directive.ts
+++ src/app/surrogate-pair-max-length.directive.ts
@@ -1,10 +1,41 @@
-import { Directive } from '@angular/core';
+import { length, substring } from 'stringz';
 
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  Renderer2
+} from '@angular/core';
+
+/**
+ * Surrogate pair max length directive.
+ *
+ * @export
+ * @class SurrogatePairMaxLengthDirective
+ */
 @Directive({
   selector: '[appSurrogatePairMaxLength]'
 })
 export class SurrogatePairMaxLengthDirective {
+  @Input() public appSurrogatePairMaxLength: number;
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  /**
+   * Handles input event.
+   *
+   * @param {*} event
+   * @memberof SurrogatePairMaxLengthDirective
+   */
+  @HostListener('input', ['$event'])
+  public onChange(event: any): void {
+    if (length(event.target.value) > this.appSurrogatePairMaxLength) {
+      this.renderer.setAttribute(
+        this.el.nativeElement,
+        'value',
+        substring(event.target.value, 0, this.appSurrogatePairMaxLength)
+      );
+    }
+  }
 }
